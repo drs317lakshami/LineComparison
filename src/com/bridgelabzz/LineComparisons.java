2f3d0590d31a648f052d4d9229feab7bd7aa9172
drs317lakshami <devrajkumar317@gmail.com>
2023-01-05 09:47:45 +0530

    Welcome to Line Comparison Programs

---
+++ src/com/bridgelabzz/LineComparisons.java
@@ -0,0 +1,16 @@
+package com.bridgelabzz;
+
+/**
+ * 
+ * @author devraj
+ *
+ */
+
+public class LineComparisons {
+	public static void main(String[] args) {
+		/*
+		 * Displaying Welcome Massage in master branch
+		 */
+		System.out.println("Welcome to Line Comparision Program");
+	}
+}
