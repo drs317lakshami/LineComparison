07696dd464092e209b3efc609def76738f194293
drs317lakshami <devrajkumar317@gmail.com>
2023-01-05 22:58:49 +0530

    Added: length Of Line as a Point

--- src/com/bridgelabzz/LineComparisons.java
+++ src/com/bridgelabzz/LineComparisons.java
@@ -10,37 +10,35 @@ import java.util.Scanner;
 
 public class LineComparisons {
 
-	public static void main(String args[]) {
-		int x1, x2, y1, y2;
+	public static void main(String args[])
+    {
+		int x1,x2,y1,y2;
 
-		double length;
+        double length;
 
-		Scanner scanner = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
 
-		System.out.println("enter x1 point");
+        System.out.println("enter x1 point");
 
-		x1 = scanner.nextInt();
+        x1=sc.nextInt();
 
-		System.out.println("enter y1 point");
+        System.out.println("enter y1 point");
 
-		y1 = scanner.nextInt();
+        y1=sc.nextInt();
 
-		System.out.println("enter x2point");
+        System.out.println("enter x2point");	
 
-		x2 = scanner.nextInt();
+        x2=sc.nextInt();
 
-		System.out.println("enter y2 point");
+        System.out.println("enter y2 point");
 
-		y2 = scanner.nextInt();
+        y2=sc.nextInt();
 
-		length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
 
-		System.out
-				.println("Length of Lines is : " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + length);
+        System.out.println("Length of Lines is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
 
-		System.out.println(" Checking Equality of Two Lengths:");
-		System.out.println(x1 + " == " + y1 + " : " + (x1 == y1));
-		System.out.println(x2 + " == " + y2 + " : " + (x2 == y2));
-	}
 
-}
+    }
+
+    }
