92592c4616fca1a8895108d7143cced4a1909f23
drs317lakshami <devrajkumar317@gmail.com>
2023-01-05 09:52:22 +0530

    Added : Calculation of length of line

--- src/com/bridgelabzz/LineComparisons.java
+++ src/com/bridgelabzz/LineComparisons.java
@@ -1,16 +1,46 @@
 package com.bridgelabzz;
 
+import java.util.Scanner;
+
 /**
  * 
- * @author devraj
+ * @author Devraj
  *
  */
 
 public class LineComparisons {
-	public static void main(String[] args) {
-		/*
-		 * Displaying Welcome Massage in master branch
-		 */
-		System.out.println("Welcome to Line Comparision Program");
+
+	public static void main(String args[]) {
+		int x1, x2, y1, y2;
+
+		double length;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("enter x1 point");
+
+		x1 = scanner.nextInt();
+
+		System.out.println("enter y1 point");
+
+		y1 = scanner.nextInt();
+
+		System.out.println("enter x2point");
+
+		x2 = scanner.nextInt();
+
+		System.out.println("enter y2 point");
+
+		y2 = scanner.nextInt();
+
+		length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+
+		System.out
+				.println("Length of Lines is : " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + length);
+
+		System.out.println(" Checking Equality of Two Lengths:");
+		System.out.println(x1 + " == " + y1 + " : " + (x1 == y1));
+		System.out.println(x2 + " == " + y2 + " : " + (x2 == y2));
 	}
+
 }
