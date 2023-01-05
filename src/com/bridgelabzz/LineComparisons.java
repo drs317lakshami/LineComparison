9b3da93459260d6f3919c0ae5d6025230b2fab3f
drs317lakshami <devrajkumar317@gmail.com>
2023-01-05 23:23:20 +0530

    Added : Line comparison

--- src/com/bridgelabzz/LineComparisons.java
+++ src/com/bridgelabzz/LineComparisons.java
@@ -8,24 +8,10 @@ import java.util.Scanner;
  *
  */
 
-	public class LineComparisons {
+public class LineComparisons {
 
-		public static void main(String[] args) {
-			System.out.println("Welcome to the Line Compression Program");
-
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter the value of X1 X2 Y1 Y2");
-
-			int x1 = sc.nextInt();
-			int x2 = sc.nextInt();
-			int y1 = sc.nextInt();
-			int y2 = sc.nextInt();
-
-			double length;
-
-			length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-			System.out.println("Length of line: " + length);
-
-		}
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Line Compression Program");
 
 	}
+}
